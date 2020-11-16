dbb96a280d769dbdd3a81a5cbd3d091e26ebd2de
Hieu Minh <hieuminh22490@gmail.com>
2020-11-16 23:11:20 +0700

    Scores App V1 - First commit

---
+++ app/src/main/java/com/hieuminh/scoresapp/view/LoginFragment.java
@@ -0,0 +1,22 @@
+package com.hieuminh.scoresapp.view;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.hieuminh.scoresapp.R;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
